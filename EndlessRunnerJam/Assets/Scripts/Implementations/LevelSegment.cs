17a47991f6029b7e3d585ca5750923a18123cb27
Matheus <matheusmsarto@gmail.com>
2025-05-04 15:23:35 -0300

    Level Loader Manager

---
+++ EndlessRunnerJam/Assets/Scripts/Implementations/LevelSegment.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class LevelSegment : MonoBehaviour
+{
+    public Transform StartPoint;
+    public Transform EndPoint;
+
+}
--- EndlessRunnerJam/Assets/Scripts/Implementations/LoadLevelsManager.cs
+++ EndlessRunnerJam/Assets/Scripts/Implementations/LoadLevelsManager.cs
@@ -1,6 +1,53 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.WSA;
 
 public class LoadLevelsManager : MonoBehaviour
 {
-    
+    [SerializeField] private Transform PlayerTransform;
+    [SerializeField] private GameObject[] SegmentPrefabs;
+    /// <summary>
+    /// How Many Segments Will Be Spawned When Game Starts
+    /// </summary>
+    [SerializeField] private int InitialSegments = 5; 
+
+
+    private List<GameObject> activeSegments = new List<GameObject>();
+
+    private float SegmentLength = 40f;   
+    private float spawnX = 0f;   
+
+
+    private void Start()
+    {
+        for (int i = 0; i < InitialSegments; i++)
+        {
+            SpawnSegment();
+        }
+    }
+
+    private void Update()
+    {
+        if(activeSegments.Count > 0 && PlayerTransform.transform.position.x > activeSegments[0].transform.position.x + SegmentLength)
+        {
+            Destroy(activeSegments[0]);
+            activeSegments.RemoveAt(0);
+
+            SpawnSegment();
+        }
+    }
+
+    // Spawns a Random Segment
+    void SpawnSegment()
+    {
+        
+        int randomIndex = Random.Range(0, SegmentPrefabs.Length);
+        GameObject segmentPrefab = SegmentPrefabs[randomIndex];
+        
+        Vector3 spawnPosition = new Vector3(spawnX, 0f, 0f);
+        spawnX += SegmentLength;
+
+        GameObject newSegment = Instantiate(segmentPrefab, spawnPosition, Quaternion.identity);
+        activeSegments.Add(newSegment);  
+    }
 }
